ae95848d50096dc4b5d1b6e46c2ebd2a58346fe8
Ava Pek <exodrifter@gmail.com>
2016-06-04 17:33:33 -0500

    Add Object Count script

---
+++ Assets/Scripts/ObjectCount.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectCount : MonoBehaviour
+{
+	public int Count {
+		get { return inside.Count; }
+	}
+
+	public List<GameObject> inside = new List<GameObject> ();
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.tag == "Piece") {
+			inside.Add(collider.gameObject);
+		}
+	}
+
+	void OnTriggerExit (Collider collider)
+	{
+		if (collider.tag == "Piece") {
+			inside.Remove(collider.gameObject);
+		}
+	}
+}
