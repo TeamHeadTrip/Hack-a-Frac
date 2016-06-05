466e8ec90b2df1b3e10bc7763e2b04e15e634f95
Ava Pek <exodrifter@gmail.com>
2016-06-04 19:07:00 -0500

    Initial level code

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic
+;
+
+public class LevelController : MonoBehaviour
+{
+	public List<ItemController> items;
+
+	public Transform hoverPos;
+
+	private ItemController currentItem;
+
+	void Start ()
+	{
+		StartLevel ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (CheckLevel ()) {
+				StartLevel ();
+			}
+		}
+	}
+
+	void StartLevel ()
+	{
+		if (items.Count > 0) {
+			currentItem = items[0];
+			currentItem.StartLevel (hoverPos);
+
+			items.RemoveAt(0);
+		}
+	}
+
+	bool CheckLevel ()
+	{
+		// Check if completed, if so...
+		if (currentItem != null) {
+			currentItem.EndLevel();
+			return true;
+		}
+		return false;
+	}
+}
